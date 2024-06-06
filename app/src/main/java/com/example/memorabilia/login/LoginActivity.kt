c09727eaf2940e2f1a013559763d1e9aa6e21226
arvinwira <arvinwira@gmail.com>
2024-06-07 00:45:46 +0700

    Revert "register + login"
    This reverts commit 87062313e3f14443cf08704d7b29479e8bf8e5a7.

--- app/src/main/java/com/example/memorabilia/login/LoginActivity.kt
+++ app/src/main/java/com/example/memorabilia/login/LoginActivity.kt
@@ -9,11 +9,6 @@ import com.example.memorabilia.ViewModelFactory
 import com.example.memorabilia.data.UserModel
 import com.example.memorabilia.databinding.ActivityLoginBinding
 import com.example.memorabilia.main.MainActivity
-import okhttp3.*
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import org.json.JSONException
-import org.json.JSONObject
-import java.io.IOException
 
 class LoginActivity : AppCompatActivity() {
     private val viewModel by viewModels<LoginViewModel> {
@@ -44,83 +39,33 @@ class LoginActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            makeLoginRequest(email, password)
+            viewModel.login(email, password) { response ->
+                if (response != null && !response.error) {
+                    alertDialog = AlertDialog.Builder(this)
+                        .setTitle("Login Successful")
+                        .setMessage("You have successfully logged in.")
+                        .setPositiveButton("OK") { _, _ -> }
+                        .show()
+
+                    viewModel.updateToken(response.loginResult.token)
+                    val user = UserModel(email, response.loginResult.token, true)
+                    viewModel.saveSession(user)
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    alertDialog = AlertDialog.Builder(this)
+                        .setTitle("Login Failed")
+                        .setMessage("Login failed. Please try again.")
+                        .setPositiveButton("OK") { _, _ -> }
+                        .show()
+                }
+            }
         }
     }
 
-    private fun makeLoginRequest(email: String, password: String) {
-        val jsonObject = JSONObject()
-        jsonObject.put("email", email)
-        jsonObject.put("password", password)
-
-        val requestBody = RequestBody.create(
-            "application/json; charset=utf-8".toMediaTypeOrNull(),
-            jsonObject.toString()
-        )
-
-        alertDialog = showLoadingDialog("Logging in...")
-
-        val request = Request.Builder()
-            .url("https://capstonebangkitc241-ps182.et.r.appspot.com/login")
-            .post(requestBody)
-            .build()
-
-        val client = OkHttpClient()
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                alertDialog?.dismiss()
-                runOnUiThread {
-                    showAlertDialog("Login failed. Please try again.")
-                }
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                alertDialog?.dismiss()
-                val responseBody = response.body?.string()
-
-                // Check if the response body is a valid JSON
-                try {
-                    JSONObject(responseBody)
-                } catch (e: JSONException) {
-                    e.printStackTrace()
-                    runOnUiThread {
-                        showAlertDialog("Invalid response. Please try again.")
-                    }
-                    return
-                }
-
-                // Proceed with converting the response to JSONObject and further processing
-                val jsonObject = JSONObject(responseBody)
-
-                if (response.isSuccessful) {
-                    val loginResult = jsonObject.optJSONObject("loginResult")
-                    if (loginResult != null) {
-                        val token = loginResult.optString("token")
-                        viewModel.updateToken(token)
-                        val user = UserModel(email, token, true)
-                        viewModel.saveSession(user)
-
-                        runOnUiThread {
-                            showAlertDialog("You have successfully logged in.")
-                            navigateToMainActivity()
-                        }
-                    } else {
-                        runOnUiThread {
-                            showAlertDialog("Login failed. Please try again.")
-                        }
-                    }
-                } else {
-                    runOnUiThread {
-                        showAlertDialog("Login failed. Please try again.")
-                    }
-                }
-            }
-        })
-    }
-
-
-
-            private fun isEmailValid(email: String): Boolean {
+    private fun isEmailValid(email: String): Boolean {
         val emailPattern = "^[A-Za-z\\d+_.-]+@[A-Za-z\\d.-]+$"
         return email.matches(emailPattern.toRegex())
     }
@@ -128,26 +73,6 @@ class LoginActivity : AppCompatActivity() {
     private fun isPasswordValid(password: String): Boolean {
         val passwordPattern = "^(?=.*[A-Z])(?=.*\\d).{8,}$"
         return password.matches(passwordPattern.toRegex())
-    }
-
-    private fun showAlertDialog(message: String) {
-        AlertDialog.Builder(this)
-            .setMessage(message)
-            .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
-            .show()
-    }
-
-    private fun showLoadingDialog(message: String): AlertDialog {
-        return AlertDialog.Builder(this)
-            .setMessage(message)
-            .setCancelable(false)
-            .show()
-    }
-
-    private fun navigateToMainActivity() {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/example/memorabilia/register/RegisterActivity.kt
+++ app/src/main/java/com/example/memorabilia/register/RegisterActivity.kt
@@ -2,22 +2,12 @@ package com.example.memorabilia.register
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import com.example.memorabilia.ViewModelFactory
 import com.example.memorabilia.databinding.ActivityRegisterBinding
 import com.example.memorabilia.login.LoginActivity
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
-import okhttp3.Response
-import okio.IOException
-import org.json.JSONObject
 
 class RegisterActivity : AppCompatActivity() {
     private val viewModel by viewModels<RegisterViewModel> {
@@ -36,73 +26,49 @@ class RegisterActivity : AppCompatActivity() {
 
     private fun setupAction() {
         binding.signupButton.setOnClickListener {
-            val username = binding.nameEditText.text.toString()
+            val name = binding.nameEditText.text.toString()
             val email = binding.emailEditText.text.toString()
             val password = binding.passwordEditText.text.toString()
 
+            if (!isEmailValid(email) || !isPasswordValid(password)) {
+                AlertDialog.Builder(this)
+                    .setTitle("Signup Failed")
+                    .setMessage("Please enter a valid email and password.")
+                    .setPositiveButton("OK") { _, _ -> }
+                    .show()
+                return@setOnClickListener
+            }
 
+            viewModel.register(name, email, password) { response ->
+                if (response != null && !response.error) {
+                    AlertDialog.Builder(this)
+                        .setTitle("Signup Successful")
+                        .setMessage("You have successfully signed up.")
+                        .setPositiveButton("OK") { _, _ -> }
+                        .show()
 
-            val jsonObject = JSONObject()
-            jsonObject.put("username", username)
-            jsonObject.put("email", email)
-            jsonObject.put("password", password)
-
-            val requestBody = RequestBody.create(
-                "application/json; charset=utf-8".toMediaTypeOrNull(),
-                jsonObject.toString()
-            )
-
-            alertDialog = showLoadingDialog("Signing up...")
-
-            val request = Request.Builder()
-                .url("https://capstonebangkitc241-ps182.et.r.appspot.com/register")
-                .post(requestBody)
-                .build()
-
-            val client = OkHttpClient()
-            client.newCall(request).enqueue(object : Callback {
-                override fun onFailure(call: Call, e: IOException) {
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
                     alertDialog?.dismiss()
-                    runOnUiThread {
-                        showAlertDialog("Signup failed. Please try again.")
-                    }
+                    finish()
+                } else {
+                    AlertDialog.Builder(this)
+                        .setTitle("Signup Failed")
+                        .setMessage("Signup failed. Please try again.")
+                        .setPositiveButton("OK") { _, _ -> }
+                        .show()
                 }
-
-                override fun onResponse(call: Call, response: Response) {
-                    alertDialog?.dismiss()
-                    if (response.isSuccessful) {
-                        runOnUiThread {
-                            showAlertDialog("You have successfully signed up.")
-                            navigateToLogin()
-                        }
-                    } else {
-                        runOnUiThread {
-                            showAlertDialog("Signup failed. Please try again.")
-                        }
-                    }
-                }
-            })
+            }
         }
     }
 
-    private fun showAlertDialog(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    private fun isEmailValid(email: String): Boolean {
+        val emailPattern = "^[A-Za-z\\d+_.-]+@[A-Za-z\\d.-]+$"
+        return email.matches(emailPattern.toRegex())
     }
 
-
-
-
-    private fun showLoadingDialog(message: String): AlertDialog {
-        return AlertDialog.Builder(this)
-            .setMessage(message)
-            .setCancelable(false)
-            .show()
+    private fun isPasswordValid(password: String): Boolean {
+        val passwordPattern = "^(?=.*[A-Z])(?=.*\\d).{8,}$"
+        return password.matches(passwordPattern.toRegex())
     }
-
-    private fun navigateToLogin() {
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
-
 }
